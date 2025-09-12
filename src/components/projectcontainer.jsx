76adccec2acc8708decc15e2d3b65c70f220b1f1
Inocencio <inocenciojose@gmail.com>
2025-09-12 10:36:40 +0100

    Adicionei estado aos projetos

--- src/components/projectcontainer.jsx
+++ src/components/projectcontainer.jsx
@@ -8,6 +8,7 @@ const projects = [
     title: "SILVA",
     image: "https://picsum.photos/400/200?random=1",
     shortDesc: "Plataforma colaborativa de segurança pública.",
+    estado: "Ainda em Desenvolvimento",
     details:
       "Plataforma colaborativa que conecta cidadãos, autoridades e tecnologia para localizar pessoas desaparecidas, rastrear foragidos e emitir alertas de segurança em tempo real. Recursos: Mapas interativos com relatórios e zonas de risco, grupos de apoio comunitário, histórias de sucesso e reportagens colaborativas. Impacto: Promove engajamento social e segurança pública por meio de uma rede integrada. Plataforma inovadora de segurança pública com mapas interativos, relatórios colaborativos e suporte comunitário."
   },
@@ -15,7 +16,8 @@ const projects = [
     id: 2,
     title: "SpotiLite-PWA",
     image: "https://picsum.photos/400/200?random=2",
-    shortDesc: "Player de música leve e moderno.",
+    shortDesc: "Player de música leve e moderno inspirado no design do Spotify.",
+    estado: "Concluído",
     details:
       "Player de música leve e moderno, inspirado no Spotify, desenvolvido como Progressive Web App (PWA) com uma API independente no backend (Node.js + Express). Oferece reprodução offline, busca dinâmica, fila de reprodução e interface responsiva com tema escuro. Player de música moderno e leve, inspirado no Spotify, funcionando como PWA com suporte offline e API independente para streaming e metadados.",
     link: "https://github.com/inocencio-jose/spotilite"
@@ -25,6 +27,7 @@ const projects = [
     title: "Orion EXCHANGER",
     image: "https://picsum.photos/400/200?random=3",
     shortDesc: "Ferramenta de conversão em lote.",
+    estado: "Concluído",
     details:
       "Ferramenta eficiente para conversão em lote de arquivos em múltiplos formatos, otimizando processos digitais. Ferramenta para conversão eficiente de arquivos em lote."
   },
@@ -33,14 +36,17 @@ const projects = [
     title: "AJ Freenet",
     image: "https://picsum.photos/400/200?random=4",
     shortDesc: "VPN rápida e segura.",
+    estado: "Concluído",
     details:
-      "VPN rápida e segura, projetada para garantir privacidade online e acesso seguro à internet. VPN focada em privacidade e segurança, garantindo acesso seguro à internet."
+      "VPN rápida e segura, projetada para garantir privacidade online e acesso seguro à internet. VPN focada em privacidade e segurança, garantindo acesso seguro à internet.",
+    link: "https://t.me/orionvpnoficial"
   },
   {
     id: 5,
     title: "SAIPH",
     image: "https://picsum.photos/400/200?random=5",
     shortDesc: "Chatbot acadêmico com Node.js e Botpress.",
+    estado: "Ainda em Desenvolvimento",
     details:
       "Chatbot acadêmico desenvolvido com Node.js e Botpress, oferecendo agendamentos automatizados, resumos personalizados e suporte educacional. Chatbot acadêmico com funcionalidades avançadas para suporte educacional."
   },
@@ -49,6 +55,7 @@ const projects = [
     title: "Landing Page Céu Digital",
     image: "https://picsum.photos/400/200?random=6",
     shortDesc: "Página informativa moderna e responsiva.",
+    estado: "Concluído",
     details:
       "Página informativa com design moderno e responsivo para a empresa Céu Digital. Página informativa com design limpo e responsivo.",
     link: "https://ceudigital.netlify.app"
@@ -58,6 +65,7 @@ const projects = [
     title: "Sistemas de Inscrição Online",
     image: "https://picsum.photos/400/200?random=7",
     shortDesc: "Plataformas acadêmicas com gestão de inscrições.",
+    estado: "Concluídos",
     details:
       "Plataformas acadêmicas com autenticação segura, gestão de inscrições e dashboards administrativos para institutos educacionais. Soluções robustas para gestão acadêmica, com autenticação segura e dashboards administrativos."
   },
@@ -66,17 +74,20 @@ const projects = [
     title: "Cosmo Clima",
     image: "https://picsum.photos/400/200?random=8",
     shortDesc: "Aplicativo de clima interativo.",
+    estado: "Concluído",
     details:
       "Aplicação de clima interativa desenvolvida com HTML, CSS, JavaScript e a API OpenWeather, fornecendo dados meteorológicos em tempo real. Aplicativo de clima com interface moderna e dados em tempo real via API OpenWeather.",
     link: "https://cosmoclima.netlify.app"
   },
   {
     id: 9,
-    title: "Bot CASSIEF",
+    title: "CASSIEF",
     image: "https://picsum.photos/400/200?random=9",
     shortDesc: "Bot multifuncional no Telegram.",
+    estado: "Concluído",
     details:
-      "Bot multifuncional em Node.js integrado à API do Telegram, criado para gerenciar o grupo da AJ Freenet com automações e interações dinâmicas. Bot em Node.js para automação e gerenciamento de comunidades no Telegram."
+      "Bot multifuncional em Node.js integrado à API do Telegram, criado para gerenciar o grupo da AJ Freenet com automações e interações dinâmicas. Bot em Node.js para automação e gerenciamento de comunidades no Telegram.",
+    link: "https://t.me/CASSIEF_bot"
   }
 ];
 
@@ -207,6 +218,7 @@ function ProjectContainer() {
               />
               <h2>{selectedProject.title}</h2>
               <p>{selectedProject.details}</p>
+              <p>Estado: <span className={selectedProject.estado}>{selectedProject.estado}</span></p>
               <a href={selectedProject.link}>{selectedProject.link}</a><br />
               <button className="btn" onClick={() => setSelectedProject(null)}>
                 Fechar Detalhes
